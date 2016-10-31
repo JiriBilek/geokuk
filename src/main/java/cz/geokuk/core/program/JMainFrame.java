93fed3581fa1b730b6518fa819f0c85ef20084db
Jiri Bilek <Jiri@bilek.info>
2016-10-31 11:53:34 +0100

    Oprava kachlovníku v rohu
    Opravuje chybu přestavování malé mapy. Nyní se malá mapa přestaví pouze,
    když uživatel nastaví leteckou mapu MapyCZ nebo google.

--- src/main/java/cz/geokuk/core/program/JMainFrame.java
+++ src/main/java/cz/geokuk/core/program/JMainFrame.java
@@ -340,8 +340,7 @@ public class JMainFrame extends JFrame implements SlideListProvider {
 		detailRoh.add(detailKachlovnik);
 		factory.init(detailKachlovnik);
 		factory.init(mysovani);
-//		detailKachlovnik.setKachloTypes(new KaSet(EnumSet.of(EKaType.OPHOTO_M)));
-		detailKachlovnik.setKachloTypes(new KaSet(EnumSet.of(EKaType.OPHOTO_GOOGLE)));
+		detailKachlovnik.setKachloTypes(new KaSet(EnumSet.of(EKaType.OPHOTO_M)));  // počáteční nastavení
 		detailRoh.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
 		return detailRoh;
 	}
--- src/main/java/cz/geokuk/plugins/mapy/kachle/JKachlovnikDoRohu.java
+++ src/main/java/cz/geokuk/plugins/mapy/kachle/JKachlovnikDoRohu.java
@@ -12,9 +12,16 @@ public class JKachlovnikDoRohu extends JKachlovnik {
 
 	@Override
 	public void setKachloTypes(final KaSet aKachloSet) {
-//		super.setKachloTypes(new KaSet(EnumSet.of(EKaType.OPHOTO_M)));
-		super.setKachloTypes(aKachloSet);
-
+//			super.setKachloTypes(new KaSet(EnumSet.of(EKaType.OPHOTO_M)));
+		
+		// Vyloučí všechno, co nejsou vybrané ortofotomapy. To, co zbyde, předá jako podklad.
+		// Když nic nezbyde, nic nedělá.
+		// Takto se zajistí, aby kachlovník do rohu se přepínal podle posledně nastavené ortofotomapy.
+		// Povolují se jenom ortofotomapy MapyCZ (aktuální) a google
+		EnumSet<EKaType> kts = aKachloSet.getKts();
+		kts.retainAll(EnumSet.of(EKaType.OPHOTO_M, EKaType.OPHOTO_GOOGLE));
+		if (! kts.isEmpty())
+			super.setKachloTypes(new KaSet(kts));
 	}
 
 }
